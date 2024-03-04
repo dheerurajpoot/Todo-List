d79e480fe7628412faca1d0370626923abcf522a
Dheeru Rajpoot <150139748+dheerurajpoot@users.noreply.github.com>
2024-03-04 13:52:16 +0530

    add local storage functionality
    add local storage functionality

--- script.js
+++ script.js
@@ -2,47 +2,69 @@ const todoInput = document.querySelector('.todoInput');
 const todoButton = document.querySelector('.todoButton');
 const todoContainer = document.querySelector('.todoContainer');
 
-    const getTodoDataFromLocalStorage = () =>{
-      return JSON.parse(localStorage.getItem("todoItemData")) || [];
-    }
+// get data from local storage
+const getTodoDataFromLocalStorage = () => {
+  return JSON.parse(localStorage.getItem("todoItemData")) || [];
+}
 
-    const addTodoDataToLocalStorage = (todo) =>{
-      localStorage.setItem("todoItemData", JSON.stringify(todo))
-    }
-  
-    const submitData = (e) => {
-      e.preventDefault();
-      let newTodo = todoInput.value.trim()
-      
+let localTodo = getTodoDataFromLocalStorage() || [];
 
-      if (newTodo === "") {
-        alert('You have to write something to add!');
-        return;
-      }
-  
-      const todoItem = document.createElement('div');
-      todoItem.classList.add('todoItem');
-      if (todoItem) {
-        todoItem.style.position = 'relative';
-      }
-  
-      const data = document.createElement('p');
-      data.innerText = newTodo;
-  
-      const deleteBtn = document.createElement('i');
-      deleteBtn.classList.add('deletebtn','fas', 'fa-trash-alt');
-      deleteBtn.addEventListener('click', () => {
-        todoContainer.removeChild(todoItem);
-      });
-  
-      todoItem.appendChild(data);
-      todoItem.appendChild(deleteBtn);
-  
-      todoContainer.appendChild(todoItem);
-  
-      todoInput.value = '';
-    }
-  
-    todoButton.addEventListener('click', (e) => {
-      submitData(e);
-    });
+// create new todo element
+const creatTodoElements = (newTodo) => {
+  const todoItem = document.createElement('div');
+  todoItem.classList.add('todoItem');
+  if (todoItem) {
+    todoItem.style.position = 'relative';
+  }
+
+  const data = document.createElement('p');
+  data.innerText = newTodo;
+
+  // create delete button and remove todo
+  const deleteBtn = document.createElement('i');
+  deleteBtn.classList.add('deletebtn', 'fas', 'fa-trash-alt');
+  deleteBtn.addEventListener('click', () => {
+    todoContainer.removeChild(todoItem);
+    removeTodo(newTodo);
+  });
+
+  todoItem.appendChild(data);
+  todoItem.appendChild(deleteBtn);
+
+  todoContainer.appendChild(todoItem);
+
+  todoInput.value = '';
+}
+
+// remove todo from local storage
+const removeTodo = (todoText) => {
+  localTodo = localTodo.filter(item => item !== todoText);
+  localStorage.setItem("todoItemData", JSON.stringify(localTodo));
+}
+
+const submitData = (e) => {
+  e.preventDefault();
+  const newTodo = todoInput.value.trim();
+
+  // send data to local storage
+  localTodo.push(newTodo)
+  localStorage.setItem("todoItemData", JSON.stringify(localTodo));
+
+  if (newTodo === "") {
+    alert('You have to write something to add!');
+    return;
+  }
+  creatTodoElements(newTodo);
+}
+
+// display data from local storage
+const displayTodo = () => {
+  localTodo.forEach((item) => {
+    creatTodoElements(item)
+  })
+}
+
+displayTodo()
+todoButton.addEventListener('click', (e) => {
+  submitData(e);
+});
